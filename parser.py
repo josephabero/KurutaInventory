4190082716b2c6523472479daade3414337998a3
Joseph Abero <30737648+josephabero@users.noreply.github.com>
2024-09-20 01:08:52 -0700

    Add scrolling display for display menus

--- parser.py
+++ parser.py
@@ -2,13 +2,12 @@ from user import KURUTA_SPREADSHEET
 
 # Script
 import csv
+import curses
 import logging
 import json
 import yaml
 
 from functools import wraps
-
-import logging
 
 def print_function_name(func):
     @wraps(func)
@@ -42,21 +41,63 @@ class KurutaParser():
 
     @print_function_name
     def display_cards(self):
-        # Header
-        title = "Displaying Cards"
-        logging.info(title)
-        logging.info("=" * len(title))
+        stdscr = curses.initscr()
+        stdscr.keypad(True)
 
-        # Display Cards
-        for card in self.cards:
-            card_content = {
-                "code": card["code"],
-                "character": card["character"],
-                "series": card["series"]
-            }
-            logging.info(yaml.dump(card_content,
-                                   indent=4,
-                                   allow_unicode=True))
+        stdscr.clear()
+        stdscr.refresh()
+
+        # Loop where k is the last character pressed
+        k = None
+        top_card_index = 0
+        while (k != ord('q')):
+            # Initialization
+            stdscr.clear()
+            height, width = stdscr.getmaxyx()
+
+            if k == curses.KEY_DOWN:
+                top_card_index += 1
+            elif k == curses.KEY_UP:
+                top_card_index = max(0, top_card_index - 1)
+
+            # Header
+            title = f"Displaying Cards: {top_card_index}"
+            stdscr.addstr(0, 0, title)
+            stdscr.addstr(1, 0, "=" * len(title))
+
+            # Display Cards
+            y = 2  # Start at 2 for Header
+            for index in range(height - 2):
+                card = self.cards[top_card_index + index]    # Use y value to determine index
+
+                card_content = {
+                    "code": card["code"],
+                    "character": card["character"],
+                    "series": card["series"]
+                }
+
+                content = f"{top_card_index + index}:"
+                stdscr.addstr(y, 0, content[:width])
+                y = y + 1
+
+                for key in card_content:
+                    content = f"  {key}: {card_content[key]}"
+                    stdscr.addstr(y, 0, content[:width])
+                    y = y + 1
+
+                    if y >= height:
+                        break
+                if y >= height:
+                        break
+
+            # Refresh the screen
+            stdscr.refresh()
+
+            # Wait for next input
+            k = stdscr.getch()
+
+        curses.endwin()
+
 
     @print_function_name
     def parse_spreadsheet(self, spreadsheet=KURUTA_SPREADSHEET):
